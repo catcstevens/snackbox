b1f9a8cd6b23084c67739b3e109bfab7569457ac
Catherine Callcott-Stevens <ccallcottstevens@gmail.com>
2019-10-31 16:24:48 +1100

    I like snacks

---
+++ index.rb
@@ -0,0 +1,11 @@
+# add and commit your changes with an appropriate commit message
+# * add some code to `index.rb` to loop through the snacks array
+# and print out a message that says "I like " followed by the name of the snack, for each snack in the array
+# * add and commit your changes with an appropriate commit message
+# * push your commits to the remote repo
+
+snacks = ["chips", "nuts", "sweets"]
+
+for i in snacks
+    puts "I like #{i}"
+end
